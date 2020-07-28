02ae0f0a5cbb407b9ff792f3ba92037371fcc98d
Jillian Rash <JillianRash@jillians-mbp.lan>
2020-07-28 09:49:07 -0500

    how to avoid meteors course

---
+++ mod/assignment/type/onlineaudio/version.php
@@ -0,0 +1,12 @@
+<?PHP
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the module version etc.
+//  This fragment is called by /admin/index.php
+////////////////////////////////////////////////////////////////////////////////
+
+$plugin->version   = 2011102008;
+$plugin->release   = "1.1.2";
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->requires  = 2010112400;
+$plugin->component = 'assignment_onlineaudio';
